e7bd0d4d1b9f5e74bdd4ec896c377ab02ebca2f2
Ali-dev <alin89328@gmail.com>
2023-09-20 15:41:59 +0500

    refactor: code refactored

--- components/jobdetails/tabs/Tabs.jsx
+++ components/jobdetails/tabs/Tabs.jsx
@@ -3,7 +3,7 @@ import { TouchableOpacity, FlatList, Text, View } from "react-native";
 import styles from "./tabs.style";
 import { SIZES } from "../../../constants";
 
-function TabButton({ name, activeTab, onHandleSearchType }) {
+const TabButton = ({ name, activeTab, onHandleSearchType }) => {
   return (
     <TouchableOpacity
       style={styles.btn(name, activeTab)}
